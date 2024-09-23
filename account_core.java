4f6f416f39ee10640f79ca23377a6ca3dca2b26f
Bruce Liu <87771104+liu-bruce@users.noreply.github.com>
2024-09-23 02:40:10 -0600

    Add files via upload

--- account_core.java
+++ account_core.java
@@ -1,12 +1,38 @@
-
-
 package account_methods;
+
+
 abstract public static class account_core
 {
-
+  private int bank_account_number = 0 ;
+  private double account_balance = 0;
+  private double interest_rate = 0;
 }
 public static class checking_account extends account_core
 {
+  public static void set_account_number()
+  {
+
+  }
+  public static void set account_balance()
+  {
+
+  }
+  public static void set_interest_rate()
+  {
+
+  }
+  public static int get_account_number()
+  {
+
+  }
+  public static double get_account_balance()
+  {
+
+  }
+  public static double get_interest_rate()
+  {
+
+  }
 
 }
 public static class savings_account extends account_core
